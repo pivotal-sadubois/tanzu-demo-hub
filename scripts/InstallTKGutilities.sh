cc5ffe99dc511b2da7ba4cfb66b4d8b043eec5ac
Sacha Dubois <sadubois@pivotal.io>
2020-11-30 20:19:05 +0100

    testing

--- scripts/InstallTKGutilities.sh
+++ scripts/InstallTKGutilities.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-TDHPATH=$1
+TDHPATH=$1; cd /tmp
 
-echo "TDHPATH:$TDHPATH"
+# INSTALL TKG UTILITY
+tar xfz $TDHPATH/software/tkg-linux-amd64-v1.2.0-vmware.1.tar.gz
+mv /usr/local/bin; chmod +x /usr/local/bin/tkg
+
+mv tkg/imgpkg-linux-amd64-v0.2.0+vmware.1 /usr/local/bin/imgpkg && chmod +x /usr/local/bin/imgpkg
+mv tkg/kapp-linux-amd64-v0.33.0+vmware.1  /usr/local/bin/kapp   && chmod +x /usr/local/bin/kapp
+mv tkg/kbld-linux-amd64-v0.24.0+vmware.1  /usr/local/bin/kbld   && chmod +x /usr/local/bin/kbld
+mv tkg/tkg-linux-amd64-v1.2.0+vmware.1    /usr/local/bin/tkg    && chmod +x /usr/local/bin/tkg
+mv tkg/ytt-linux-amd64-v0.30.0+vmware.1   /usr/local/bin/ytt    && chmod +x /usr/local/bin/ytt
+
+# INSTALL TKG EXTENSIONS
+mkdir $TDHPATH/extensions && cd $TDHPATH/extensions
+tar xfz $TDHPATH/software/tkg-extensions-manifests-v1.2.0-vmware.1.tar-2.gz
+
+touch  /tkg_software_installed
