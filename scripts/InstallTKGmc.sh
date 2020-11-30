6f89901a3365d24cf7213c9ee0e583abedfd5cf6
Sacha Dubois <sadubois@pivotal.io>
2020-11-30 21:18:30 +0100

    testing

---
+++ scripts/InstallTKGmc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ############################################################################################
+# File: ........: deployTKGmc
+# Language .....: bash
+# Author .......: Sacha Dubois, VMware
+# Description ..: Tanzu Demo Hub - Deploy TKG Management Cluster
+# ############################################################################################
+
+export TANZU_DEMO_HUB=$(cd "$(pwd)/$(dirname $0)/.."; pwd)
+export TDHPATH=$(cd "$(pwd)/$(dirname $0)/.."; pwd)
+export DEPLOY_TKG_TEMPLATE=$1
+
+echo "DEPLOY_TKG_TEMPLATE:$DEPLOY_TKG_TEMPLATE"
+echo "TDHPATH:$TDHPATH"
+
+. $TANZU_DEMO_HUB/functions
+
+# --- VERIFY DEPLOYMENT ---
+if [ ! -f ${TDHPATH}/deployments/${DEPLOY_TKG_TEMPLATE} ]; then
+  echo "ERROR: Deployment file $DEPLOY_TKG_TEMPLATE can not be found in ${TDHPATH}/deployments"
+  exit 1
+else
+  . ${TDHPATH}/deployments/${DEPLOY_TKG_TEMPLATE}
+fi
+
+createCluster 
+
