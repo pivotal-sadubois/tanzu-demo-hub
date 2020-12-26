b12d6ae2754be245f7e2cc41c03ec9e717de37ee
Sacha Dubois <sadubois@pivotal.io>
2020-12-26 21:10:17 +0100

    testing

--- scripts/uploadOVAimages.sh
+++ scripts/uploadOVAimages.sh
@@ -51,6 +51,8 @@ messageTitle "Uploading OVS Images to vSphere"
 for n in $(echo $TDH_TKGMC_TKG_IMAGES | awk -F'/' '{ print $2 }'); do
   pth=$(echo $n | sed -e 's/-vmware.1.ova//g' -e 's/+vmware.1.ova//g') 
   cnt=$(govc datastore.ls -ds=$VSPHERE_DATASTORE | grep -c "$pth")
+echo "PTH:$pth"
+echo "N:$n"
   if [ $cnt -eq 0 ]; then
     stt="uploaded"
     $OVFTOOL $OVFOPTS software/${n} $OVFCONN
