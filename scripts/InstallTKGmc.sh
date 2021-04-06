889ca4b306a0195db13d75f15caaa7b92f24d067
Sacha Dubois <sadubois@pivotal.io>
2021-04-06 22:12:40 +0200

    testing

--- scripts/InstallTKGmc.sh
+++ scripts/InstallTKGmc.sh
@@ -10,9 +10,7 @@ export TANZU_DEMO_HUB=$(cd "$(pwd)/$(dirname $0)/.."; pwd)
 export TDHPATH=$(cd "$(pwd)/$(dirname $0)/.."; pwd)
 export DEPLOY_TKG_TEMPLATE=$1
 
-echo ggaga1
 . $TANZU_DEMO_HUB/functions
-echo ggaga2
 
 # --- VERIFY DEPLOYMENT ---
 if [ ! -f ${TDHPATH}/deployments/${DEPLOY_TKG_TEMPLATE} ]; then
@@ -21,13 +19,11 @@ if [ ! -f ${TDHPATH}/deployments/${DEPLOY_TKG_TEMPLATE} ]; then
 else
   . ${TDHPATH}/deployments/${DEPLOY_TKG_TEMPLATE}
 fi
-echo ggaga3
 
 # --- CHECK ENVIRONMENT VARIABLES ---
 if [ -f ~/.tanzu-demo-hub.cfg ]; then
   . ~/.tanzu-demo-hub.cfg
 fi
-echo ggaga4
 
 export TDH_DEPLOYMENT_ENV_NAME=$TDH_TKGMC_INFRASTRUCTURE
 export TKG_CONFIG=${TDHPATH}/config/$TDH_TKGMC_CONFIG
@@ -38,11 +34,8 @@ if [ "${TDH_DEPLOYMENT_ENV_NAME}" == "Azure" ]; then
   az vm image terms accept --publisher vmware-inc --offer tkg-capi --plan k8s-1dot19dot3-ubuntu-1804 --subscription $AZURE_SUBSCRIPTION_ID > /dev/null 2>&1
   az vm image terms accept --publisher vmware-inc --offer tkg-capi --plan k8s-1dot20dot4-ubuntu-2004 --subscription $AZURE_SUBSCRIPTION_ID > /dev/null 2>&1
 fi
-echo ggaga5
 
 #sshEnvironment > /dev/null 2>&1
-echo ggaga6
-hostname
 createCluster 
 
 
