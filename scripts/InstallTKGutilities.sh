c1ca1b202c4e235c7da69a130b5bc83463303bb9
Sacha Dubois <sadubois@pivotal.io>
2021-04-17 10:03:04 +0200

    added comments

--- scripts/InstallTKGutilities.sh
+++ scripts/InstallTKGutilities.sh
@@ -3,24 +3,7 @@
 TDHPATH=$1; cd /tmp
 TDHENV=$2; cd /tmp
 
-## INSTALL TKG UTILITY
-#TKG_ARCHIVE=$(ls -1 $TDHPATH/software/tkg-linux* | tail -1) 
-#tar xfz $TKG_ARCHIVE
-#
-#ls -la ./tkg
-#if [ ! -d ./tkg ]; then 
-#  echo "ERROR: failed to unpack $TKG_ARCHIVE"
-#  echo "       tar xfz $TKG_ARCHIVE"
-#  exit
-#fi
-
-#mv tkg/imgpkg-linux-amd64-* /usr/local/bin/imgpkg && chmod +x /usr/local/bin/imgpkg
-#mv tkg/kapp-linux-amd64-*   /usr/local/bin/kapp   && chmod +x /usr/local/bin/kapp
-#mv tkg/kbld-linux-amd64-*   /usr/local/bin/kbld   && chmod +x /usr/local/bin/kbld
-#mv tkg/tkg-linux-amd64-*    /usr/local/bin/tkg    && chmod +x /usr/local/bin/tkg
-#mv tkg/ytt-linux-amd64-*    /usr/local/bin/ytt    && chmod +x /usr/local/bin/ytt
-
-## INSTALL TKG EXTENSIONS
+echo "=> Install TKG Extensions"
 #mkdir -p $TDHPATH/extensions && cd $TDHPATH/extensions
 #tar xfz $TDHPATH/software/tkg-extensions-manifests-v1.2.0-vmware.1.tar-2.gz
 #sudo chown -R ubuntu:ubuntu $TDHPATH/extensions
@@ -31,8 +14,8 @@ sudo systemctl start docker > /dev/null 2>&1
 sudo systemctl enable docker > /dev/null 2>&1
 sudo usermod -aG docker ubuntu
 
-# INSTALL TANZU
 if [ ! -f /usr/local/bin/vmw-cli ]; then
+  echo "=> Install (vmw-cli)"
   #sudo apt install npm -y > /dev/null 2>&1
   #npm install vmw-cli --global > /dev/null 2>&1
   docker run apnex/vmw-cli shell > vmw-cli 
@@ -41,6 +24,7 @@ if [ ! -f /usr/local/bin/vmw-cli ]; then
 fi
 
 if [ ! -f /usr/local/bin/tanzu ]; then
+  echo "=> Install Tanzu CLI"
   . ~/.tanzu-demo-hub.cfg
   export VMWUSER="$TDH_MYVMWARE_USER"
   export VMWPASS="$TDH_MYVMWARE_PASS"
@@ -50,12 +34,10 @@ if [ ! -f /usr/local/bin/tanzu ]; then
   vmwfile=$(vmw-cli ls vmware_tanzu_kubernetes_grid 2>/dev/null | egrep "^tanzu-cli-bundle-linux" | tail -1 | awk '{ print $1 }')
   while [ "$vmwfile" == "" -a $cnt -lt 5 ]; do
     vmwfile=$(vmw-cli ls vmware_tanzu_kubernetes_grid 2>/dev/null | egrep "^tanzu-cli-bundle-linux" | tail -1 | awk '{ print $1 }')
-echo "$cnt $vmwfile"
     let cnt=cnt+1
     sleep 10
   done
 
-echo "vmwfile:$vmwfile"
   (cd /tmp/; vmw-cli cp $vmwfile > /dev/null 2>&1)
   cd /tmp; tar xf $vmwfile
 
@@ -75,15 +57,18 @@ fi
 
 
 # INSTALL KIND
+echo "=> Install Kind Cluster"
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64 2>/dev/null
 chmod +x ./kind
 mv kind /usr/local/bin
 
 # --- INSTALL KUNEADM ---
+echo "=> Install Kubeadm"
 sudo snap install kubeadm --classic
 
 if [ "$TDHENV" == "vSphere" ]; then 
   if [ ! -f /usr/bin/ovftool ]; then 
+    echo "=> Install ovftool"
     if [ -f $TDHPATH/software/VMware-ovftool-4.4.1-16812187-lin.x86_64.bundle ]; then 
       echo -e "\n\n\nyes" | sudo nohup $TDHPATH/software/VMware-ovftool-4.4.1-16812187-lin.x86_64.bundle
     else
@@ -93,7 +78,7 @@ if [ "$TDHENV" == "vSphere" ]; then
     fi
   fi
   
-  # INSTALL GOVC
+  echo "=> Install GOVC"
   sudo apt install golang-go -y > /dev/null 2>&1
   sudo apt install gccgo-go -y > /dev/null 2>&1
   #curl -L https://github.com/vmware/govmomi/releases/download/v0.24.0/govc_linux_amd64.gz --output govc_linux_amd64.gz > /dev/null 2>&1
