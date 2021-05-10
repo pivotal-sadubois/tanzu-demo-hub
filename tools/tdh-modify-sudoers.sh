7af6c4e1af23738036430fb009f3489b9fc3eec2
Sacha Dubois <sadubois@pivotal.io>
2021-05-10 23:30:58 +0200

    testing

--- tools/tdh-modify-sudoers.sh
+++ tools/tdh-modify-sudoers.sh
@@ -3,5 +3,5 @@
 USER=$1
 
 echo > /dev/null 2>&1
-echo "$USER    ALL=(ALL:ALL) ALL" > /etc/sudoers.d/$USER && chmod 440 /etc/sudoers.d/$USER
-sed -i 's/#includedir/includedir/' /etc/sudoers
+echo "$USER    ALL=(ALL:ALL) ALL" > /etc/sudoers.d/$USER && chmod 0440 /etc/sudoers.d/$USER
+#sed -i 's/#includedir/includedir/' /etc/sudoers.d
