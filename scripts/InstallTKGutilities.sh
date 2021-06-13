d2021d15c2764410ba32d20a67b5ac53bcdb62a8
Sacha Dubois <sadubois@pivotal.io>
2021-06-14 00:49:39 +0200

    fix permission issue with docker

--- scripts/InstallTKGutilities.sh
+++ scripts/InstallTKGutilities.sh
@@ -35,9 +35,14 @@ while [ $ret -ne 0 -a $cnt -lt 5 ]; do
   let cnt=cnt+1
 done
 
-echo "docker login $TDH_REGISTRY_DOCKER_NAME -u $TDH_REGISTRY_DOCKER_USER -p $TDH_REGISTRY_DOCKER_PASS "
-echo $ret
+# --- VERIFY RATE-LIMIT ---
+TOKEN=$(curl --user '$TDH_REGISTRY_DOCKER_USER:$TDH_REGISTRY_DOCKER_PASS' "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
+rlm=$(curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest | grep "ratelimit-limit" | awk '{ print $NF }') 
+rrm=$(curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest | grep "ratelimit-remaining" | awk '{ print $NF }') 
+messagePrint " ▪ Docker Ratelimit:" "$rlm"
+messagePrint " ▪ Docker Ratelimit Remaining:" "$rrm"
 
+echo "docker login $TDH_REGISTRY_DOCKER_NAME -u $TDH_REGISTRY_DOCKER_USER -p $TDH_REGISTRY_DOCKER_PASS "
 if [ $ret -ne 0 ]; then
   echo "ERROR: failed to login to docker"
   echo "       => docker login $TDH_REGISTRY_DOCKER_NAME -u $TDH_REGISTRY_DOCKER_USER -p $TDH_REGISTRY_DOCKER_PASS"
