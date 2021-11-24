1fe16d06576f7cf0a5c00d1f1b8868910016c65e
Sacha Dubois <sadubois@pivotal.io>
2021-11-24 01:15:49 +0100

    testing

--- scripts/InstallUtilities.sh
+++ scripts/InstallUtilities.sh
@@ -27,10 +27,19 @@ installPackage snapd
 installPackage curl
 
 if [ ! -f /usr/bin/docker ]; then 
-  apt-get remove docker docker-engine docker.io containerd runc -y > /dev/null 2>&1
-  #installPackage docker.io
-  installSnap docker
-  ln -s /snap/bin/docker /usr/bin/docker
+  # 20. Nov sdubois moved from snapd to apt install docker
+  apt install apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+  apt update
+  installPackage docker.io
+  sudo chmod 777 /var/run/docker.sock
+
+#  apt-get remove docker docker-engine docker.io containerd runc -y > /dev/null 2>&1
+#  #installPackage docker.io
+#  installSnap docker
+#  ln -s /snap/bin/docker /usr/bin/docker
+
   groupadd docker
   usermod -aG docker ubuntu
 fi
