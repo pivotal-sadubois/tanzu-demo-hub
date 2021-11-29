060484891c2da8f5f6225b4adc00ec21824c5a90
Sacha Dubois <sadubois@pivotal.io>
2021-11-29 21:09:12 +0100

    fixing tanzu cli

--- tools/tdh-tools.sh
+++ tools/tdh-tools.sh
@@ -63,17 +63,32 @@ fi
 # --- DUMP ENVIRONMENT VARIABLES ---
 env | grep TDH > /tmp/tdh.env
 
+# /Users/sdu/.tanzu/config.yaml
+# /Users/sdu/.config/tanzu/config.yaml
+
 if [ $ROOT_SHELL -eq 0 ]; then 
+  [ ! -d $HOME/.tanzu ] && mkdir -p $HOME/.tanzu
+
   docker run -it --rm --name tdh-tools -v /var/run/docker.sock:/var/run/docker.sock tdh-tools:latest  chmod 666 /var/run/docker.sock > /dev/null 2>&1
   docker run -u $(id -u):$(id -g) -it --rm --name tdh-tools --env-file /tmp/tdh.env \
-     -v $HOME:$HOME:ro -v $HOME/.local:$HOME/.local:rw -v $HOME/.tanzu-demo-hub:$HOME/.tanzu-demo-hub:rw \
-     -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.cache:$HOME/.cache:rw -v $HOME/.config:$HOME/.config:rw \
-     -v $HOME/.aws:$HOME/.aws:rw -v $HOME/.vmware-cna-saas:$HOME/.vmware-cna-saas:rw -v $HOME/.azure:$HOME/.azure:rw \
-     -v /tmp:/tmp:rw -v /tmp/docker:$HOME/.docker:rw -v $HOME/.mc:$HOME/.mc:rw -v $HOME/.tanzu:$HOME/.tanzu:rw \
+     -v $HOME:$HOME:ro -v $HOME/.tanzu-demo-hub:$HOME/.tanzu-demo-hub:rw \
+     -v $HOME/.config:$HOME/.config:rw -v $HOME/.config_tools/tanzu:$HOME/.config/tanzu:rw\
+     -v $HOME/.tanzu_tools:$HOME/.tanzu:rw -v $HOME/.cache_tools:$HOME/.cache:rw \
+     -v /var/run/docker.sock:/var/run/docker.sock \
+     -v $HOME/.vmware-cna-saas:$HOME/.vmware-cna-saas:rw -v $HOME/.azure:$HOME/.azure:rw \
+     -v /tmp:/tmp:rw -v /tmp/docker:$HOME/.docker:rw -v $HOME/.mc:$HOME/.mc:rw \
      -v $HOME/.kube-tkg:$HOME/.kube-tkg:rw -v $HOME/.kube:$HOME/.kube:rw -v $HOME/.govmomi:$HOME/.govmomi:rw \
      -v $HOME/.ssh:$HOME/.ssh:rw -v $HOME/.terraform:$HOME/.terraform:rw -v $HOME/.gradle:$HOME/.gradle:rw \
-     -v $HOME/.s3cfg:$HOME/.s3cfg:rw \
+     -v $HOME/.s3cfg:$HOME/.s3cfg:rw -v $HOME/.local_tools:$HOME/.local:rw \
      -e "KUBECONFIG=$HOME/.kube/config" --hostname tdh-tools tdh-tools:latest $COMMAND
+
+     #-v $HOME/.tanzu_tools:$HOME/.tanzu:rw -v $HOME/.config_tools:$HOME/.config:rw -v $HOME/.cache_tools:$HOME/.cache:rw \
+     #-v $HOME/.tanzu_tools:$HOME/.tanzu:rw -v $HOME/.config_tools:$HOME/.config:rw -v $HOME/.cache_tools:$HOME/.cache:rw \
+     #-v $HOME/.tanzu_tools:$HOME/.tanzu:rw -v $HOME/.tanzu_tools:$HOME/.config:rw -v $HOME/.cache_tools:$HOME/.cache:rw \
+     #-v $HOME:$HOME:ro -v $HOME/.tanzu-demo-hub:$HOME/.tanzu-demo-hub:rw \
+     #-v $HOME/.tanzu-demo-hub:$HOME/.tanzu-demo-hub:rw $HOME/.cache/helm:$HOME/.cache/helm:rw \
+     #-v $HOME:$HOME:ro -v $HOME/.local:$HOME/.local:rw -v $HOME/.tanzu-demo-hub:$HOME/.tanzu-demo-hub:rw \
+     #-v $HOME/.tanzu-demo-hub:$HOME/.tanzu-demo-hub:rw -v $HOME/.cache/helm:$HOME/.cache/helm:rw \
 else
   docker run -it --rm --name tdh-tools -v /var/run/docker.sock:/var/run/docker.sock tdh-tools:latest  chmod 666 /var/run/docker.sock > /dev/null 2>&1
   docker run -it --rm --name tdh-tools --env-file /tmp/tdh.env \
@@ -89,4 +104,18 @@ fi
 
 exit
 
+ls -la ~/.tanzu ~/.config ~/.cache
+ls -la ~/.tanzu_tools ~/.config_tools ~/.cache_tools
+
+tanzu plugin list
+tanzu plugin repo  add --local cli core
+itanzu plugin repo  add --local cli
+
+tanzu plugin install --local cli all
+tanzu plugin list
+tanzu plugin clean
+
+sdu@tdh-tools:/tanzu$ tanzu plugin repo delete core
+sdu@tdh-tools:/tanzu$ tanzu plugin repo delete tkg 
+sdu@tdh-tools:/tanzu$ tanzu plugin repo list  
 
