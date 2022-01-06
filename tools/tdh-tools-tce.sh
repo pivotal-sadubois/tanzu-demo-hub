0b6c9429f86ab1cb1ef00de8f9e66d1ff61f0c95
Sacha Dubois <sadubois@pivotal.io>
2022-01-06 20:02:52 +0100

    test

--- tools/tdh-tools-tce.sh
+++ tools/tdh-tools-tce.sh
@@ -99,11 +99,8 @@ CORE_MOUNTS=(
 
 # --- MAKE SURE DIRECTORIES ARE CREATED ---
 for n in $(echo ${CORE_MOUNTS[*]} | sed 's/\-. //g'); do
-echo "N:$n"
   localdir=$(echo $n | awk -F: '{ print $1 }')
-echo "   localdir:$localdir"
-  [ $n == "/var/run/docker.sock" ] && continue
-echo gaga
+  [ $localdir == "/var/run/docker.sock" ] && continue
   [ ! -d $localdir -a ! -f $localdir ] && mkdir -p $localdir
 done
 
