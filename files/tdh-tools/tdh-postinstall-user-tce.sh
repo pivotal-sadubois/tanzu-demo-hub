702158007a80b63945f019028db443e69be81b85
Sacha Dubois <sadubois@pivotal.io>
2022-01-10 20:20:06 +0100

    changed post install scripts

--- files/tdh-tools/tdh-postinstall-user-tce.sh
+++ files/tdh-tools/tdh-postinstall-user-tce.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-/usr/local/bin/tanzu > /dev/null 2>&1; ret=$?
-
-#if [ ! -f $HOME/.tanzu/config.yaml ]; then 
-if [ $ret -ne 0 ]; then 
+/usr/local/bin/tanzu plugin list > /dev/null 2>&1; ret=$?
+stt=$(/usr/local/bin/tanzu plugin list -o json | jq -r '.[] | select(.name == "management-cluster").status') 
+if [ $ret -ne 0 -o "$stt" == "not installed" ]; then 
   # --- INSTALL TANZU UTILITIES ---
   /tanzu/tce-linux-amd64/install.sh 
   #cd /tanzu/tce-linux-amd64 && tanzu plugin clean                                                               <
--- files/tdh-tools/tdh-postinstall-user.sh
+++ files/tdh-tools/tdh-postinstall-user.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if [ ! -d $HOME/.tanzu ]; then 
+#!/bin/bash
+
+/usr/local/bin/tanzu plugin list > /dev/null 2>&1; ret=$?
+stt=$(/usr/local/bin/tanzu plugin list -o json | jq -r '.[] | select(.name == "management-cluster").status')
+if [ $ret -ne 0 -o "$stt" == "not installed" ]; then
   cd /tanzu && tanzu plugin clean
   cd /tanzu && tanzu plugin install --local cli all
 fi
