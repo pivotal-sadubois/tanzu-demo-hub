e842b539f4658ec1912ef0be3d9cb553ae514eee
Sacha Dubois <sadubois@pivotal.io>
2022-01-20 17:55:57 +0100

    fix bug with kubernetes config

--- files/tdh-tools/tdh-postinstall-user-tce.sh
+++ files/tdh-tools/tdh-postinstall-user-tce.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+chown -R ${id -u):${id -g) $HOME > /dev/null 2>&1
+
 /usr/local/bin/tanzu plugin list > /dev/null 2>&1; ret=$?
 stt=$(/usr/local/bin/tanzu plugin list -o json | jq -r '.[] | select(.name == "management-cluster").status') 
 if [ $ret -ne 0 -o "$stt" == "not installed" ]; then 
