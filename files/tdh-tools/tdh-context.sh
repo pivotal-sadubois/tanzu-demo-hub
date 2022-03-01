013e7e727df7d34fba60bd7e56eea6e3a81a84f6
Sacha Dubois <sadubois@pivotal.io>
2022-03-01 11:43:31 +0100

    testing tce

--- files/tdh-tools/tdh-context.sh
+++ files/tdh-tools/tdh-context.sh
@@ -2,39 +2,49 @@
 
 CONTEXT_LIST=""
 # --- GATHER RIGHT KUBECONFIG ---
-for n in $(ls -1 $HOME/.tanzu-demo-hub/config/tkgmc-vsphere*.kubeconfig); do
+for n in $(ls -1 $HOME/.tanzu-demo-hub/config/*.kubeconfig | egrep "tkgmc|tcemc"); do
   nam=$(echo $n | sed 's/kubeconfig/cfg/g')
+  vsp=$(echo $n | egrep -c "tkgmc-vsphere|tcemc-vsphere") 
   [ -s $nam ] && . ${nam}   ## READ ENVIRONMENT VARIABLES FROM CONFIG FILE
 
-  cnt=$(echo $TDH_TKGMC_SUPERVISORCLUSTER | egrep -c "pez.vmware.com") 
-  if [ $cnt -gt 0 ]; then 
-    curl -m 3 https://pez-portal.int-apps.pcfone.io > /dev/null 2>&1; ret=$?
-    if [ $ret -eq 0 ]; then
+  if [ $csp -gt 0 ]; then 
+    cnt=$(echo $TDH_TKGMC_SUPERVISORCLUSTER | egrep -c "pez.vmware.com") 
+    if [ $cnt -gt 0 ]; then 
+      curl -m 3 https://pez-portal.int-apps.pcfone.io > /dev/null 2>&1; ret=$?
+      if [ $ret -eq 0 ]; then
+        [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
+  
+        export KUBECTL_VSPHERE_PASSWORD=$TDH_TKGMC_VSPHERE_PASS
+        kubectl vsphere login --insecure-skip-tls-verify --server $TDH_TKGMC_SUPERVISORCLUSTER -u $TDH_TKGMC_VSPHERE_USER > /tmp/error.log 2>&1; ret=$?
+  
+        [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
+        mv $HOME/.kube/config.old $HOME/.kube/config
+  
+        kubectl --kubeconfig=$n get ns >/dev/null 2>&1; ret=$?
+        if [ $ret -eq 0 ]; then
+          nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
+          CONTEXT_LIST="$CONTEXT_LIST $nam:$n"
+        fi
+      else
+        echo "ERROR: Can not verify $nam as connection to VMware VPN is required"
+      fi
+    else
       [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
-
+  
       export KUBECTL_VSPHERE_PASSWORD=$TDH_TKGMC_VSPHERE_PASS
       kubectl vsphere login --insecure-skip-tls-verify --server $TDH_TKGMC_SUPERVISORCLUSTER -u $TDH_TKGMC_VSPHERE_USER > /tmp/error.log 2>&1; ret=$?
-
+  
       [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
       mv $HOME/.kube/config.old $HOME/.kube/config
-
+  
       kubectl --kubeconfig=$n get ns >/dev/null 2>&1; ret=$?
       if [ $ret -eq 0 ]; then
         nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
         CONTEXT_LIST="$CONTEXT_LIST $nam:$n"
       fi
-    else
-      echo "ERROR: Can not verify $nam as connection to VMware VPN is required"
     fi
   else
-    [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
-
-    export KUBECTL_VSPHERE_PASSWORD=$TDH_TKGMC_VSPHERE_PASS
-    kubectl vsphere login --insecure-skip-tls-verify --server $TDH_TKGMC_SUPERVISORCLUSTER -u $TDH_TKGMC_VSPHERE_USER > /tmp/error.log 2>&1; ret=$?
-
-    [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
-    mv $HOME/.kube/config.old $HOME/.kube/config
-
+    # --- REGULAR CLUSTER (NOT-VSPHERE) ----
     kubectl --kubeconfig=$n get ns >/dev/null 2>&1; ret=$?
     if [ $ret -eq 0 ]; then
       nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
@@ -43,7 +53,7 @@ for n in $(ls -1 $HOME/.tanzu-demo-hub/config/tkgmc-vsphere*.kubeconfig); do
   fi
 done
 
-for n in $(ls -1 $HOME/.tanzu-demo-hub/config/tdh*.kubeconfig); do
+for n in $(ls -1 $HOME/.tanzu-demo-hub/config/tdh*.kubeconfig 2>/dev/null); do
   nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
 
   kubectl --kubeconfig=$n get cm -n default -o json > /tmp/output.json 2>/dev/null
