2873d5cf6d2ba3abad4730a111d453f7658d3779
Sacha Dubois <sadubois@pivotal.io>
2022-03-05 00:02:45 +0100

    copy /Users/sdu/.tanzu-demo-hub/cache directory to jump server

--- scripts/buildTDHToolsContainer.sh
+++ scripts/buildTDHToolsContainer.sh
@@ -17,17 +17,10 @@ export TDHPATH=$(cd "$(pwd)/$(dirname $0)/.."; pwd)
 export TKG_TYPE=$1
 export TKG_RELEASE=$2
 
-echo "buildTDHToolsContainer.sh gaga2"
 [ -f $TANZU_DEMO_HUB/functions ] && . $TANZU_DEMO_HUB/functions
-echo "buildTDHToolsContainer.sh gaga3"
 [ -f $HOME/.tanzu-demo-hub.cfg ] && . $HOME/.tanzu-demo-hub.cfg
-echo "PCF_PIVNET_TOKEN:$PCF_PIVNET_TOKEN"
-grep PCF_PIVNET_TOKEN $HOME/.tanzu-demo-hub.cfg
-echo "buildTDHToolsContainer.sh gaga4"
 
 for rel in $(ls -1 $TDHPATH/files/tdh-tools/tdh-tools-tkg-*.cfg | sed -e 's/^.*tools-tkg-//g' -e 's/\.cfg//g'); do
-echo "buildTDHToolsContainer.sh gaga5 TKG_TYPE:$TKG_TYPE TKG_RELEASE:$TKG_RELEASE"
   tdh_tools_build $TKG_TYPE $TKG_RELEASE
-echo "buildTDHToolsContainer.sh gaga6"
 done
 
