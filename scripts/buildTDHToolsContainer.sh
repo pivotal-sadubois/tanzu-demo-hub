013b2bc0558f3e972d9192520c70f25121d6ca08
Sacha Dubois <sadubois@pivotal.io>
2022-03-05 23:02:42 +0100

    tdh-tools container für TKGmc deploymnet

--- scripts/buildTDHToolsContainer.sh
+++ scripts/buildTDHToolsContainer.sh
@@ -6,6 +6,9 @@
 # Description ..: Tanzu Demo Hub - Create TDH Tools Container
 # ############################################################################################
 
+echo ppppp1
+hostname
+echo ppppp2
 export TANZU_DEMO_HUB=$(cd "$(pwd)/$(dirname $0)/.."; pwd)
 export TDHPATH=$(cd "$(pwd)/$(dirname $0)/.."; pwd)
 export DEPLOY_TKG_TEMPLATE=tkgmc-dev-vsphere-macbook.cfg
@@ -19,8 +22,6 @@ export TKG_RELEASE=$2
 [ -f $TANZU_DEMO_HUB/functions ] && . $TANZU_DEMO_HUB/functions
 [ -f $HOME/.tanzu-demo-hub.cfg ] && . $HOME/.tanzu-demo-hub.cfg
 
-for rel in $(ls -1 $TDHPATH/files/tdh-tools/tdh-tools-tkg-*.cfg | sed -e 's/^.*tools-tkg-//g' -e 's/\.cfg//g'); do
-  tdh_tools_build $TKG_TYPE $TKG_RELEASE
-done
+tdh_tools_build $TKG_TYPE $TKG_RELEASE
 
 exit 0
