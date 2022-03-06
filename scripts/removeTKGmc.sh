6b02a9b73b89b490cfed4b5f1ad1f4e63e44d159
Sacha Dubois <sadubois@pivotal.io>
2022-03-06 23:00:36 +0100

    tesing

--- scripts/removeTKGmc.sh
+++ scripts/removeTKGmc.sh
@@ -17,14 +17,17 @@ export TDH_TOOLS_CONTAINER_TYPE="$4"
 export DEPLOY_TKG_VERSION="$5"
 export NATIVE=0
 
+echo removeTKGmc.sh-gaga2
 # --- SETTING FOR TDH-TOOLS ---
 export START_COMMAND="$*"
 export CMD_EXEC=scripts/$(basename $0)
 export CMD_ARGS=$*
 
+echo removeTKGmc.sh-gaga3
 # --- SOUTCE FOUNCTIONS AND USER ENVIRONMENT ---
 [ -f $TANZU_DEMO_HUB/functions ] && . $TANZU_DEMO_HUB/functions
 [ -f $HOME/.tanzu-demo-hub.cfg ] && . $HOME/.tanzu-demo-hub.cfg
+echo removeTKGmc.sh-gaga4
 
 #############################################################################################################################
 ################################### EXECUTING CODE WITHIN  TDH-TOOLS DOCKER CONTAINER  ######################################
@@ -34,11 +37,15 @@ runTDHtools $TDH_TOOLS_CONTAINER_TYPE $DEPLOY_TKG_VERSION "Deploy TKG Management
 # --- RESET TDH_TKGMC_NAME ---
 export TDH_TKGMC_NAME="$TDH_TKGMC_NAME_TMP"
 
+echo removeTKGmc.sh-gaga5
 cnt=$(tanzu cluster list --include-management-cluster 2>/dev/null | grep -c " $TDH_TKGMC_NAME")
+echo removeTKGmc.sh-gaga5
 if [ $cnt -gt 0 ]; then
+echo removeTKGmc.sh-gaga6
   messageTitle "Deleting Management Cluster ($TDH_TKGMC_NAME)"
   tanzu management-cluster delete $TDH_TKGMC_NAME -y > /tmp/error.log 2>&1; ret=$?
  
+echo removeTKGmc.sh-gaga7
   if [ $ret -ne 0 ]; then
     logMessages /tmp/error.log
     echo "ERROR: failed to delete management cluster ($TDH_TKGMC_NAME)"
@@ -52,9 +59,11 @@ if [ $cnt -gt 0 ]; then
 
     exit 1
   fi
+echo removeTKGmc.sh-gaga8
 
   [ -f $HOME/.tanzu-demo-hub/config/$TDH_TKGMC_NAME.kubeconfig ] && rm -f $HOME/.tanzu-demo-hub/config/$TDH_TKGMC_NAME.kubeconfig
   [ -f $HOME/.tanzu-demo-hub/config/$TDH_TKGMC_NAME.cfg ] && rm -f $HOME/.tanzu-demo-hub/config/$TDH_TKGMC_NAME.cfg
   [ -f $HOME/.tanzu-demo-hub/config/$TDH_TKGMC_NAME.yaml ] && rm -f $HOME/.tanzu-demo-hub/config/$TDH_TKGMC_NAME.yaml
+echo removeTKGmc.sh-gaga9
 fi
 
