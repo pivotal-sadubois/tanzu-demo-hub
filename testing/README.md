768d60f633165d852860134ea18dadb2d27ec721
Sacha Dubois <sdubois@vmware.com>
2022-03-12 19:15:27 +0100

    test routinges for attaching/dettaching of workload clustert to TMC

--- testing/README.md
+++ testing/README.md
@@ -2,3 +2,6 @@
 This tools are for developement only and are for testing to attach workload cluster to TMC and 
 detach it afterwards. Pleae do not use these scripts.
 ##############################################################################################
+
+test-tmc-attach-wc.sh            ## Test the attaching of a TKG Workload Cluster to TMC
+test-tmc-dattach-wc.sh           ## Test the dettaching of a TKG Workload Cluster to TMC
--- testing/test-tmc-dettach-wc.sh
+++ testing/test-tmc-dettach-wc.sh
@@ -1,14 +1,19 @@
 # ############################################################################################
-# File: ........: test-tmc-dettach-vsphere-wc.sh
+# File: ........: test-tmc-dettach-wc.sh
 # Language .....: bash
 # Author .......: Sacha Dubois, VMware
-# Description ..: Tanzu Demo Hub - Test TMC Dettaching of a TKG Workload Cluster on vSphere
+# Description ..: Tanzu Demo Hub - Test TMC Dettaching of a TKG Workload Cluster
 # ############################################################################################
 [ "$(hostname)" != "tdh-tools" ] && echo "ERROR: Needs to run in a tdh-tools container" && exit
 
+if [ "$1" == "" ]; then 
+  echo "Usage: $0 <tkg-workload-cluster>"
+  exit 1
+else
+  CLUSTER=$1
+fi
+
 cd /Users/sdu/workspace/tanzu-demo-hub
 . ./functions
-
-CLUSTER=tdh-vsphere-sadubois
 
 detachClusterFromTMConFailure $CLUSTER attached attached force
