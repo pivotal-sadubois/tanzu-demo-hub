b1acea4102b8d13ebcf1b9e0a39ae8086abd29bb
Sacha Dubois <sdubois@vmware.com>
2022-03-14 00:55:25 +0100

    testing fix for issue #9: Postgres Demo failed to create singleton database on TCE Workload Cluster

--- demos/tanzu-data-postgres/tanzu-postgres-deploy-singleton.sh
+++ demos/tanzu-data-postgres/tanzu-postgres-deploy-singleton.sh
@@ -97,6 +97,8 @@ execCmd "kubectl create namespace $NAMESPACE"
 execCmd "kubectl get namespace"
 
 # --- GET THE KUBERNETES DEFAULT STORAGE CLASSE ---
+cmdLoop kubectl get sc -o json > /tmp/output.json
+STORAGE_CLASS=$(jq -r '.items[].metadata | select(.annotations."storageclass.kubernetes.io/is-default-class" == "true").name' /tmp/output.json)
 STORAGE_CLASS=$(kubectl get sc | grep default | awk '{ print $1 }') 
 [ "$STORAGE_CLASS" == "" ] && STORAGE_CLASS=standard
 
