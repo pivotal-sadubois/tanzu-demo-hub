8c0c3b518289027b7b7c3e60dc290f0f184a51ca
Sacha Dubois <sdubois@vmware.com>
2022-05-13 08:55:35 +0200

    fix for vsphere cluster login in tdh-tools container

--- files/tdh-tools/tdh-context.sh
+++ files/tdh-tools/tdh-context.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONTEXT_LIST=""; CONTEXT_BAD
+CONTEXT_LIST=""; CONTEXT_BAD=""
 # --- GATHER RIGHT KUBECONFIG ---
 for n in $(ls -1 $HOME/.tanzu-demo-hub/config/*.kubeconfig 2>/dev/null | egrep "tkgmc|tcemc"); do
   nam=$(echo $n | sed 's/kubeconfig/cfg/g')
@@ -34,7 +34,7 @@ for n in $(ls -1 $HOME/.tanzu-demo-hub/config/*.kubeconfig 2>/dev/null | egrep "
 
       cnt=$(echo $TDH_TKGMC_SUPERVISORCLUSTER | egrep -c "h2o.vmware.com")               
       if [ $cnt -gt 0 ]; then
-        curl -m 3 https://h2o.vmware.com > /dev/null 2>&1; ret=$?
+        curl -k -m 3 https://h2o.vmware.com > /dev/null 2>&1; ret=$?
         if [ $ret -eq 0 ]; then
           [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
  
@@ -84,6 +84,20 @@ done
 
 for n in $(ls -1 $HOME/.tanzu-demo-hub/config/tdh*.kubeconfig 2>/dev/null); do
   nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
+  cnt=$(grep -c "TDH_TKGMC_NAME=tkgmc-vsphere" $HOME/.tanzu-demo-hub/config/${nam}.cfg) 
+  if [ $cnt -gt 0 ]; then
+    . $HOME/.tanzu-demo-hub.cfg
+    . $HOME/.tanzu-demo-hub/config/${nam}.cfg
+
+    [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
+    export KUBECTL_VSPHERE_PASSWORD=$TDH_TKGMC_VSPHERE_PASS
+    export KUBECONFIG=$HOME/.kube/config
+    kubectl vsphere login --insecure-skip-tls-verify --server $VSPHERE_TKGS_SUPERVISOR_CLUSTER \
+          -u $VSPHERE_TKGS_VCENTER_ADMIN --tanzu-kubernetes-cluster-name $nam \
+          --tanzu-kubernetes-cluster-namespace $TDH_TKGMC_VSPHERE_NAMESPACE > /dev/null 2>&1; ret=$?
+    [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
+    [ -s $HOME/.kube/config.old ] && mv $HOME/.kube/config.old $HOME/.kube/config
+  fi
 
   kubectl --kubeconfig=$n --request-timeout 3s get cm -n default -o json > /tmp/output.json 2>/dev/null; ret=$?
   if [ $ret -eq 0 ]; then
@@ -113,7 +127,7 @@ for n in $CONTEXT_LIST; do
   nam=$(echo $n | awk -F: '{ print $1 }')
   pth=$(echo $n | awk -F: '{ print $2 }')
 
-  printf " export KUBECONFIG=%-80s   ## %s\n" $pth $nam
+  printf " export KUBECONFIG=%-82s   ## %s\n" $pth $nam
   export KUBECONFIG=$pth
 done
 
