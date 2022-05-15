67b024f5c1944aa8aed4b5e5af45a219de44d52b
Sacha Dubois <sdubois@vmware.com>
2022-05-15 13:25:34 +0200

    changes for TAP

--- files/tdh-tools/tdh-postinstall-user-tkg-1.5.2.sh
+++ files/tdh-tools/tdh-postinstall-user-tkg-1.5.2.sh
@@ -2,5 +2,6 @@
 
 chown -R $(id -u):$(id -g) $HOME > /dev/null 2>&1
 
-cnt=$(/usr/local/bin/tanzu plugin list | grep management-cluster | grep -c "not installed") 
-[ $cnt -gt 0 ] && /usr/local/bin/tanzu plugin sync > /dev/null 2>&1
+#export TANZU_CLI_NO_INIT=true
+[ ! -d $HOME/.local/share/tanzu-cli/management-cluster ] && /usr/local/bin/tanzu plugin sync > /dev/null 2>&1
+[ ! -d $HOME/.local/share/tanzu-cli/accelerator ] && cd /tanzu-tap && tanzu plugin install --local cli all > /dev/null 2>&1
