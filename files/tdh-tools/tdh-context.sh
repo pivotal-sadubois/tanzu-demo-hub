4ab47c3c3fd9abe54f0e7f117bab85863ce72842
Sacha Dubois <sdubois@vmware.com>
2022-05-16 07:24:52 +0200

    TAP 1.0.3 => 1.1.0

--- files/tdh-tools/tdh-context.sh
+++ files/tdh-tools/tdh-context.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+TIMEOUT=3s
+
+# ------------------------------------------------------------------------------------------
+# Function Name ......: verify_vpn
+# Function Purpose ...: Verify VPN Access for PEZ/H2O Demo Environments
+# ------------------------------------------------------------------------------------------
+# Argument ($1) ......: Supervisor Cluster
+# ------------------------------------------------------------------------------------------
+# Return Value .......: 0=No VPN Required, 1=VPN Required but not connectec
+# ------------------------------------------------------------------------------------------
+verify_vpn() {
+  vpn=0
+  CLUSTER=$1
+  
+  cnt=$(echo $CLUSTER | egrep -c "pez.vmware.com") 
+  if [ $cnt -gt 0 ]; then  
+    curl -m 3 https://pez-portal.int-apps.pcfone.io > /dev/null 2>&1; ret=$?
+    [ $ret -ne 0 ] && vpn=1
+  fi
+
+  cnt=$(echo $CLUSTER | egrep -c "h2o.vmware.com")
+  if [ $cnt -gt 0 ]; then
+    curl -k -m 3 https://h2o.vmware.com > /dev/null 2>&1; ret=$?
+    [ $ret -ne 0 ] && vpn=1
+  fi
+
+  echo $vpn
+}
+
+#########################################################################################################################
+##################################### VERIFY MANAGEMENT / SUPERVISOR CLUSTER ############################################
+#########################################################################################################################
 CONTEXT_LIST=""; CONTEXT_BAD=""
 # --- GATHER RIGHT KUBECONFIG ---
 for n in $(ls -1 $HOME/.tanzu-demo-hub/config/*.kubeconfig 2>/dev/null | egrep "tkgmc|tcemc"); do
@@ -8,71 +40,31 @@ for n in $(ls -1 $HOME/.tanzu-demo-hub/config/*.kubeconfig 2>/dev/null | egrep "
   [ -s $nam ] && . ${nam}   ## READ ENVIRONMENT VARIABLES FROM CONFIG FILE
 
   if [ $vsp -gt 0 ]; then 
-    cnt=$(echo $TDH_TKGMC_SUPERVISORCLUSTER | egrep -c "pez.vmware.com|h2o.vmware.com") 
-    if [ $cnt -gt 0 ]; then 
-      cnt=$(echo $TDH_TKGMC_SUPERVISORCLUSTER | egrep -c "pez.vmware.com") 
-      if [ $cnt -gt 0 ]; then 
-        curl -m 3 https://pez-portal.int-apps.pcfone.io > /dev/null 2>&1; ret=$?
-        if [ $ret -eq 0 ]; then
-          [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
-  
-          export KUBECTL_VSPHERE_PASSWORD=$TDH_TKGMC_VSPHERE_PASS
-          kubectl vsphere login --insecure-skip-tls-verify --server $TDH_TKGMC_SUPERVISORCLUSTER -u $TDH_TKGMC_VSPHERE_USER > /tmp/error.log 2>&1; ret=$?
-  
-          [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
-          [ -s $HOME/.kube/config.old ] && mv $HOME/.kube/config.old $HOME/.kube/config
-  
-          kubectl --kubeconfig=$n --request-timeout 3s get ns >/dev/null 2>&1; ret=$?
-          if [ $ret -eq 0 ]; then
-            nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
-            CONTEXT_LIST="$CONTEXT_LIST $nam:$n"
-          fi
-        else
-          echo "ERROR: Can not verify $nam as connection to VMware VPN is required"
-        fi
-      fi
-
-      cnt=$(echo $TDH_TKGMC_SUPERVISORCLUSTER | egrep -c "h2o.vmware.com")               
-      if [ $cnt -gt 0 ]; then
-        curl -k -m 3 https://h2o.vmware.com > /dev/null 2>&1; ret=$?
-        if [ $ret -eq 0 ]; then
-          [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
- 
-          export KUBECTL_VSPHERE_PASSWORD=$TDH_TKGMC_VSPHERE_PASS
-          kubectl vsphere login --insecure-skip-tls-verify --server $TDH_TKGMC_SUPERVISORCLUSTER -u $TDH_TKGMC_VSPHERE_USER > /tmp/error.log 2>&1; ret=$?
- 
-          [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
-          [ -s $HOME/.kube/config.old ] && mv $HOME/.kube/config.old $HOME/.kube/config
- 
-          kubectl --kubeconfig=$n --request-timeout 3s get ns >/dev/null 2>&1; ret=$?
-          if [ $ret -eq 0 ]; then
-            nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
-            CONTEXT_LIST="$CONTEXT_LIST $nam:$n"
-          fi
-        else
-          echo "ERROR: Can not verify $nam as connection to VMware VPN is required"
-        fi
-      fi
-    else
+    # --- SKIP TEST IF VPN IS NOT ENABLED ---
+    vpn=$(verify_vpn $TDH_TKGMC_SUPERVISORCLUSTER) 
+    if [ $vpn -eq 0 ]; then
       [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
   
+      export KUBECONFIG=$HOME/.kube/config
       export KUBECTL_VSPHERE_PASSWORD=$TDH_TKGMC_VSPHERE_PASS
       kubectl vsphere login --insecure-skip-tls-verify --server $TDH_TKGMC_SUPERVISORCLUSTER -u $TDH_TKGMC_VSPHERE_USER > /tmp/error.log 2>&1; ret=$?
   
       [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
       [ -s $HOME/.kube/config.old ] && mv $HOME/.kube/config.old $HOME/.kube/config
   
-      kubectl --kubeconfig=$n --request-timeout 3s get ns >/dev/null 2>&1; ret=$?
+      kubectl --kubeconfig=$n --request-timeout $TIMEOUT get ns >/dev/null 2>&1; ret=$?
       if [ $ret -eq 0 ]; then
         nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
         CONTEXT_LIST="$CONTEXT_LIST $nam:$n"
       else
-        CONTEXT_BAD="$CONTEXT_BAD $nam:$n"
+	CONTEXT_BAD="$CONTEXT_BAD $nam:$n"
       fi
+    else
+      echo "ERROR: Can not verify $nam as connection to VMware VPN is required"
     fi
   else
     # --- REGULAR CLUSTER (NOT-VSPHERE) ----
-    kubectl --kubeconfig=$n --request-timeout 1s get ns >/dev/null 2>&1; ret=$?
+    kubectl --kubeconfig=$n --request-timeout $TIMEOUT get ns >/dev/null 2>&1; ret=$?
     if [ $ret -eq 0 ]; then
       nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
       CONTEXT_LIST="$CONTEXT_LIST $nam:$n"
@@ -82,24 +74,40 @@ for n in $(ls -1 $HOME/.tanzu-demo-hub/config/*.kubeconfig 2>/dev/null | egrep "
   fi
 done
 
+#########################################################################################################################
+############################################### VERIFY TKG CLUSTERS #####################################################
+#########################################################################################################################
 for n in $(ls -1 $HOME/.tanzu-demo-hub/config/tdh*.kubeconfig 2>/dev/null); do
   nam=$(echo $n | awk -F'/' '{ print $NF }' | sed 's/\.kubeconfig//g')
-  cnt=$(grep -c "TDH_TKGMC_NAME=tkgmc-vsphere" $HOME/.tanzu-demo-hub/config/${nam}.cfg) 
-  if [ $cnt -gt 0 ]; then
-    . $HOME/.tanzu-demo-hub.cfg
-    . $HOME/.tanzu-demo-hub/config/${nam}.cfg
+  vsp=$(grep -c "TDH_TKGMC_NAME=tkgmc-vsphere" $HOME/.tanzu-demo-hub/config/${nam}.cfg 2>/dev/null) 
+  [ "$vsp" == "" ] && vsp=0
 
-    [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
-    export KUBECTL_VSPHERE_PASSWORD=$TDH_TKGMC_VSPHERE_PASS
-    export KUBECONFIG=$HOME/.kube/config
-    kubectl vsphere login --insecure-skip-tls-verify --server $VSPHERE_TKGS_SUPERVISOR_CLUSTER \
+  if [ $vsp -gt 0 ]; then
+    [ -s $HOME/.tanzu-demo-hub.cfg ] && . $HOME/.tanzu-demo-hub.cfg
+    [ -s $HOME/.tanzu-demo-hub/config/${nam}.cfg ] && . $HOME/.tanzu-demo-hub/config/${nam}.cfg
+
+    # --- SKIP TEST IF VPN IS NOT ENABLED ---
+    vpn=$(verify_vpn $TDH_TKGMC_SUPERVISORCLUSTER)
+    if [ $vpn -eq 0 ]; then
+      [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config.old
+
+      # --- LOGIN TO THE SUPERVISOR CLUSTER FIRST ---
+      export KUBECONFIG=$HOME/.kube/config
+      export KUBECTL_VSPHERE_PASSWORD=$VSPHERE_TKGS_VCENTER_PASSWORD
+
+      kubectl vsphere login --insecure-skip-tls-verify --server $VSPHERE_TKGS_SUPERVISOR_CLUSTER \
           -u $VSPHERE_TKGS_VCENTER_ADMIN --tanzu-kubernetes-cluster-name $nam \
           --tanzu-kubernetes-cluster-namespace $TDH_TKGMC_VSPHERE_NAMESPACE > /dev/null 2>&1; ret=$?
-    [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
-    [ -s $HOME/.kube/config.old ] && mv $HOME/.kube/config.old $HOME/.kube/config
+
+      [ $ret -ne 0 ] && CONTEXT_BAD="$CONTEXT_BAD $nam:$n"
+      [ -s $HOME/.kube/config ] && mv $HOME/.kube/config $n
+      [ -s $HOME/.kube/config.old ] && mv $HOME/.kube/config.old $HOME/.kube/config
+    else
+      echo "ERROR: Can not verify $nam as connection to VMware VPN is required"
+    fi
   fi
 
-  kubectl --kubeconfig=$n --request-timeout 3s get cm -n default -o json > /tmp/output.json 2>/dev/null; ret=$?
+  kubectl --kubeconfig=$n --request-timeout $TIMEOUT get cm -n default -o json > /tmp/output.json 2>/dev/null; ret=$?
   if [ $ret -eq 0 ]; then
     if [ -s /tmp/output.json ]; then
       cfm=$(jq -r '.items[].metadata | select(.name == "tanzu-demo-hub").name' /tmp/output.json 2>/dev/null)
