31240fbea2b9021ee9440bdcb4f64440c2c24f06
Sacha Dubois <sdubois@vmware.com>
2023-04-12 11:33:04 +0200

    new

---
+++ scripts/dockerPullSecret.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ############################################################################################
+# File: ........: dockerPullSecret.sh
+# Language .....: bash
+# Author .......: Sacha Dubois, VMware
+# Cathegory ....: TAP
+# Description ..: Tanzu Demo Hub - Installation Tanzu TKG utilities on Jump Host
+# ############################################################################################
+[ "$(hostname)" != "tdh-tools" ] && echo "ERROR: Need to run within a tdh-tools container" && exit
+
+export TDHHOME=$HOME/tanzu-demo-hub
+export NAMESPACE=$1
+
+if  [ "$NAMESPACE" == "" ]; then
+  echo "Usage: $0 <namespace>"; exit 1
+fi
+
+[ -f $TDHHOME/functions ] && . $TDHHOME/functions
+[ -f $HOME/.tanzu-demo-hub.cfg ] && . $HOME/.tanzu-demo-hub.cfg
+
+dockerPullSecret $NAMESPACE
