8719bbb61362f9d1936b12f3907feb43b86b7b46
Sacha Dubois <sdubois@vmware.com>
2023-05-09 20:57:40 +0200

    new

--- tools/tdh-tools.sh
+++ tools/tdh-tools.sh
@@ -1,79 +1,39 @@
 #!/bin/bash
 # ############################################################################################
-# File: ........: tdh-tools.sh
+# File: ........: tdh-tools-cleanup.sh
 # Language .....: bash
 # Author .......: Sacha Dubois, VMware
-# Description ..: Tanzu Demo Hub - TDH Tools Container
+# Description ..: Tanzu Demo Hub - TDH Tools Container Cleanup
 # ############################################################################################
-[ "$(hostname)" == "tdh-tools" ] && echo "ERROR: Can not be run within the tdh-tools container" && exit
 
-export TANZU_DEMO_HUB=$(cd "$(pwd)/$(dirname $0)/../"; pwd)
-export TDHPATH=$(cd "$(pwd)/$(dirname $0)/../"; pwd)
-export ROOT_SHELL=0
-export COMMAND=bash
-export COMMAND=/usr/local/bin/tdh-context.sh
-export SILENT=0
-export TDH_TOOLS=tdh-tools-tkg
-export TKG_VERSION=1.6.1
-
-# --- SETTING FOR TDH-TOOLS ---
-export NATIVE=0                ## NATIVE=1 r(un on local host), NATIVE=0 (run within docker)
-export START_COMMAND="$*"
-export CMD_EXEC=$(basename $0)
-export CMD_ARGS=$*
+TANZU_DEMO_HUB=$(cd "$(pwd)/$(dirname $0)/../"; pwd)
+TDHPATH=$(cd "$(pwd)/$(dirname $0)/../"; pwd)
+VERSION=$(echo $0 | sed -e 's/^.*cleanup-//g' -e 's/\.sh//g') 
+TYPE=$(echo $0 | sed -e 's/-cleanup//g' -e 's/\.sh//g' | awk -F'/' '{ print $NF }') 
 
 # --- SOUTCE FOUNCTIONS AND USER ENVIRONMENT ---
 [ -f $TANZU_DEMO_HUB/functions ] && . $TANZU_DEMO_HUB/functions
-[ -f $HOME/.tanzu-demo-hub.cfg ] && . $HOME/.tanzu-demo-hub.cfg
 
-while [ "$1" != "" ]; do
-  case $1 in
-    --usage)  usage;;
-    --help)   usage;;
-    --root)   ROOT_SHELL=1;;
-    --cmd)    COMMAND="$2";;
-    --debug)  DEBUG=1;;
-    --silent) SILENT=1;;
-    --version) VERSION=$2
-  esac
-  shift
+tdhHeader "Tanzu Demo Hub - TDH Tools Container Cleanup ($TYPE)"
+
+echo "WARNING: This will cleanup the temporary files (dotfiles) of the TDH Tools Container ($TYPE)"
+echo "         Access to all active Tanzu Demo Hub deployments will be lost. The files and"
+echo "         directory will be recreated the next time $TYPE wil be restarted."
+echo ""
+
+if [ -d $HOME/.${TYPE} ]; then 
+  file $HOME/.${TYPE}/.* | awk -F: '{ print $1 }' | sed 1,2d | sed 's/^/         => /g'
+fi
+
+echo ""
+x=""
+while [ "$x" != "y" -a "$x" != "n" ]; do
+  echo -e "Are you shure to delete thous files <y/n> ?: \c"; read x
 done
 
-#############################################################################################################################
-################################### EXECUTING CODE WITHIN  TDH-TOOLS DOCKER CONTAINER  ######################################
-#############################################################################################################################
-runTDHtools tkg $TKG_VERSION "Run TDH Tools Docker Container" "$COMMAND" ""
-
-usage() {
-  echo "USAGE: $0 [oprions] <deployment>"
-  echo "                   --usage     # Show this info"
-  echo "                   --help      # Show this info"
-  echo "                   --root      # Get a Root Shell"
-  echo "                   --debug     # Show Debugging information"
-  echo "                   --cmd       # Execute a command"
-  exit
-}
-
-if [ $SILENT -eq 1 ]; then 
-  tdh_tools_build    tkg > /dev/null 2>&1
-  tdh_tools_download tkg > /dev/null 2>&1
-  checkExecutionLock tdh-tools > /dev/null 2>&1
-else
-  echo ""
-  echo "Tanzu Demo Hub - TDH Tools Docker Container"
-  echo "by Sacha Dubois, VMware Inc,"
-  echo "-----------------------------------------------------------------------------------------------------------"
-  echo ""
-
-  checkCLIcommands   BASIC
-  tdh_tools_build    tkg
-  tdh_tools_download tkg
-  checkExecutionLock tdh-tools
+if [ "$x" != "y" ]; then 
+  [ "${TYPE}" != "" -a "$HOME" != "" ] && rm -rf $HOME/.${TYPE}
 fi
 
-if [ $? -ne 0 ]; then 
-  echo "ERROR: $0 is already running, plese stop it first"
-  exit 1
-fi
 
-exit 0
+
