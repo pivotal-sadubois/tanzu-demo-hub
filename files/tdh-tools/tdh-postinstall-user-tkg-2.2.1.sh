2145d0e74154961dad47977a2ce9ed0beca9ce1d
Sacha Dubois <sdubois@vmware.com>
2023-08-07 12:56:19 +0200

    fix bug with tanzu plugin installation

--- files/tdh-tools/tdh-postinstall-user-tkg-2.2.1.sh
+++ files/tdh-tools/tdh-postinstall-user-tkg-2.2.1.sh
@@ -8,3 +8,35 @@ if [ $(stat -c "%U" $HOME/.ssh) == "root" ]; then
   chown -R $(id -u):$(id -g) $HOME > /dev/null 2>&1
 fi
 
+# --- ACCEPT EULA ---
+tanzu config eula accept > /dev/null 2>&1
+
+# --- INSTALL TAP PLUGINS ---
+if [ ! -d $HOME/.local/share/tanzu-cli/management-cluster ]; then 
+  echo "## Installing tanzu-tkg cli plugins, this may take a while ..."
+  cnt=0; ret=1
+  while [ ${ret} -ne 0 -a ${cnt} -lt 3 ]; do
+    tanzu plugin install --group vmware-tkg/default 2>/dev/null; ret=$?
+    [ $ret -eq 0 ] && break
+    cnt=cnt+1
+  done
+fi
+
+# --- INSTALL TAP PLUGINS ---
+if [ ! -d $HOME/.local/share/tanzu-cli/services ]; then 
+  echo "## Installing tanzu-tap v1.6.1 cli plugins, this may take a while ..."
+  cnt=0; ret=1
+  while [ ${ret} -ne 0 -a ${cnt} -lt 3 ]; do
+    tanzu plugin install --group vmware-tap/default:v1.6.1 2>/dev/null; ret=$?
+    [ $ret -eq 0 ] && break
+    cnt=cnt+1
+  done
+fi
+
+cnt=0; ret=1
+while [ ${ret} -ne 0 -a ${cnt} -lt 3 ]; do
+  tanzu plugin sync > /dev/null 2>&1; ret=$?
+  [ $ret -eq 0 ] && break
+  cnt=cnt+1
+done
+
